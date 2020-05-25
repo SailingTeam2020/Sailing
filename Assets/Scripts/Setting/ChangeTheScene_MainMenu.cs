a282ca6854059db44a60081d0038fb1fd6cb8f4f
ishikura <ishikura.yda@gmail.com>
2020-05-25 18:10:07 +0900

    [add] ゲーム内メニュー画面の完成
    歯車マークを押すとオプションとリスタートとゲーム終了の案内が完成
    先輩がボタンを画像で作っていたのでそのまま画像を使いました
    オプションの内容がまだ決まってないのでメインメニューの設定画面のままです

---
+++ Assets/Scripts/Setting/ChangeTheScene_MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Common;
+
+
+namespace Sailing
+{
+    public class ChangeTheScene_MainMenu : MonoBehaviour
+    {
+        //[SerializeField] private string str = null;
+
+        private void Start()
+        {
+
+            //if(str == null) { Debug.LogError("未設定です。"); }
+            GetComponent<Button>().onClick.AddListener(() => SceneSwitchMainMenu(SceneNameString.MainMenu));
+
+        }
+
+        public void SceneSwitchMainMenu(string str)
+        {
+            FadeManager.FadeOut(str);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Setting/PauseManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseManager : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void onClick()
+    {
+        //Time.timeScale=0の場合停止する
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
