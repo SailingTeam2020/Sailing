181f7afdbacb7135112020b62a46b4710d938584
nexus7Z3Yuki <yda.nexus7.yuki@gmail.com>
2020-05-28 14:29:02 +0900

    [add]集中線の追加

--- Assets/Scripts/InGame/Ship/ShipBubbles.cs
+++ Assets/Scripts/InGame/Ship/ShipBubbles.cs
@@ -11,6 +11,7 @@ namespace Sailing.Online
         ParticleSystem.EmissionModule mEmObj2;
         ParticleSystem.EmissionModule mEmObj3;
 
+        ParticleSystem.MainModule mainobj;
         private ShipMove shipMove;
         private float speed;
 
@@ -20,12 +21,11 @@ namespace Sailing.Online
             ParticleSystem ParticleObj1 = transform.Find("Particle1").GetComponent<ParticleSystem>();
             //↓最終目的である rateOverTime にアクセスするために必要な emission を取得し格納
             mEmObj1 = ParticleObj1.emission;
-
             ParticleSystem ParticleObj2 = transform.Find("Particle2").GetComponent<ParticleSystem>();
             mEmObj2 = ParticleObj2.emission;
-
             ParticleSystem ParticleObj3 = transform.Find("Particle3").GetComponent<ParticleSystem>();
             mEmObj3 = ParticleObj3.emission;
+
             shipMove = GameObject.Find("Ship").GetComponent<ShipMove>();
             speed = 0;
         }
@@ -35,12 +35,13 @@ namespace Sailing.Online
         private float mCount = 0;       //←時間計測用
         void Update()
         {
-             speed = shipMove.MoveSpeed;
+
+            speed = shipMove.MoveSpeed;
             mCount = mCount + Time.deltaTime;   //←時間計測中
             if (mCount >= 1.0f)
             { //経過する度に if 成立
                 mCount = 0; // 時間計測用変数を初期化
-                    //スピードに対してN個放出する
+                            //スピードに対してN個放出する
                     mEmObj1.rateOverTime = new ParticleSystem.MinMaxCurve(speed);
                     mEmObj2.rateOverTime = new ParticleSystem.MinMaxCurve(speed);
                     mEmObj3.rateOverTime = new ParticleSystem.MinMaxCurve(speed);
