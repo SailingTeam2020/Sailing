fdae0b51986eee43d1ca61dfa65005cfd1ea0e09
ishikura <ishikura.yda@gmail.com>
2020-07-03 09:20:29 +0900

    [update] 動画再生機能完成

---
+++ Assets/Scripts/InGame/MovieControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class MovieControl : MonoBehaviour
+{
+    public VideoClip videoClip;
+    public GameObject screen;
+
+    void Start()
+    {
+        var videoPlayer = screen.AddComponent<VideoPlayer>();   // videoPlayeコンポーネントの追加
+
+        videoPlayer.source = VideoSource.VideoClip; // 動画ソースの設定
+        videoPlayer.clip = videoClip;
+
+        videoPlayer.isLooping = true;   // ループの設定
+    }
+
+    public void VPControl()
+    {
+        var videoPlayer = GetComponent<VideoPlayer>();
+
+        if (!videoPlayer.isPlaying) // ボタンを押した時の処理
+            videoPlayer.Play(); // 動画を再生する。
+        else
+            videoPlayer.Pause();    // 動画を一時停止する。
+    }
+}
