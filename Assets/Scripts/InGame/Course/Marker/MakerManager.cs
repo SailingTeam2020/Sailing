17a1f0392e01f4828519e104843893dee521219a
nexus7Z3Yuki <yda.nexus7.yuki@gmail.com>
2020-07-09 17:16:47 +0900

    [add]コースをランダムで生成するを追加。

--- Assets/Scripts/InGame/Course/Marker/MakerManager.cs
+++ Assets/Scripts/InGame/Course/Marker/MakerManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
+using System;
 namespace Sailing
 {
 
@@ -24,30 +25,45 @@ namespace Sailing
             MakerObjectList = new Dictionary<int, MakerObject>();
 
         }
-
+        private int MarkerValueRandom;//マーカーの生成箇所
+        private int MarkerNumberRandom;//コースの長さ
         // Start is called before the first frame update
         void Start()
         {
 
             MakerFactory makerFactory = gameObject.AddComponent<MakerFactory>();
             CourseData course = GetComponent<CourseManager>().CourseData;
-            int maxMakerNum = course.makerTransformList.Count;
-            for (int i = 0; i < maxMakerNum; i++)
+            List<MakerTransform> makerList = course.makerTransformList;
+            int maxMakerNum = makerList.Count;
+
+            //最小値が3つ最大はマーカー座標の登録数(制限をかけるかも)
+            MarkerNumberRandom = UnityEngine.Random.Range(3, makerList.Count);
+            Debug.Log("マーカー数 " + MarkerNumberRandom);
+
+            //Randomにシャッフルして、上からMarkerNumberRandom分取り出す。
+            var RandomList = Enumerable.Range(0, makerList.Count).OrderBy(n => Guid.NewGuid()).Take(MarkerNumberRandom).ToArray();
+               //マーカーの順番の確認用
+            
+                for (int s = 0; s < RandomList.Length; s++)
+                {
+                    Debug.Log(RandomList[s]);
+                }
+             
+                for (int i = 0; i < MarkerNumberRandom; i++)
             {
 
                 GameObject obj;
-
                 MakerNum++;
                 bool isGoal = false;
                 if(i == maxMakerNum - 1)
                 {
-                    obj = makerFactory.Create(true, course.makerTransformList[i]);
+                    obj = makerFactory.Create(true, makerList[RandomList[i]]);
                     
                     isGoal = true;
                 }
                 else
                 {
-                    obj = makerFactory.Create(false, course.makerTransformList[i]);
+                    obj = makerFactory.Create(false, makerList[RandomList[i]]);
                 }
 
                 MakerObject maker = obj.GetComponent<MakerObject>();
