cb45fc8f7d19381d8c9e0299df08a4bd33940a4e
nezumi3030 <nezumi3014@gmail.com>
2020-08-28 09:26:58 +0900

    [updata]スピード表記削除、ゲージを船のスピードに対応

--- Assets/Scripts/InGame/UI/WindInfluenceUI.cs
+++ Assets/Scripts/InGame/UI/WindInfluenceUI.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Sailing
@@ -6,7 +7,7 @@ namespace Sailing
 
     public class WindInfluenceUI : MonoBehaviour
     {
-
+        //値でゲージの長さの可変を行う
         [SerializeField]
         private ShipManager shipManager;
         [SerializeField]
@@ -16,18 +17,36 @@ namespace Sailing
 
         private void Start()
         {
-
+            // shipManager内、MainShipObject内のShipMoveクラスを取得
             shipMove = shipManager.MainShipObject.GetComponent<ShipMove>();
 
         }
 
         void Update()
         {
+            /*************************************
+               小出 7/6着手
 
-            windSlider.value = shipMove.WindInfluence;
-        
+            *************************************/
+            // shipMoveのWindInfluenceの値をwindSlider.valueに代入
+            windSlider.value = shipMove.WindInfluence;  //揚力の計算？
+
+            //7/24　足立から指示
+            windSlider.value = shipMove.MoveSpeed;  //船の速さを代入
+            //windSlider.value = GetAllWindForce();
+
+        }
+        /*************************************
+        private float GetAllWindForce()
+        {
+            NewMethod();
         }
 
+        private static void NewMethod()
+        {
+            throw new NotImplementedException();
+        }
+            *************************************/
     }
 
 }
