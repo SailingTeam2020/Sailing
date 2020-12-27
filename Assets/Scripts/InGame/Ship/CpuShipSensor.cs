caf1bb67be6bdadd383e8b39d296ba250d43a5dd
phylos096 <43868600+phylos096@users.noreply.github.com>
2020-12-27 13:34:13 +0900

    [add]CpuShipSensorを追加

---
+++ Assets/Scripts/InGame/Ship/CpuShipSensor.cs
@@ -0,0 +1,31 @@
+﻿using Sailing;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CpuShipSensor : MonoBehaviour
+{
+    GameObject cpuship;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Ship") || collider.gameObject.CompareTag("Player"))
+        {
+            transform.parent.GetComponent<CpushipMove>().CollisionDetected(this);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        transform.parent.GetComponent<CpushipMove>().CollisionExitDetected(this);
+    }
+}
