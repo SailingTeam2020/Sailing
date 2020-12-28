8226c13ba4435d6d5021512932aac23979d52d25
gai99721 <kgai.yda@gmail.com>
2020-12-28 14:44:46 +0900

    [update] RoomID表示のバグ修正

--- Assets/Scripts/Online/InRoom/RoomIDUI.cs
+++ Assets/Scripts/Online/InRoom/RoomIDUI.cs
@@ -16,11 +16,24 @@ namespace Sailing.Online
         [SerializeField]
         Text RoomID;
 
+        public string roomID
+        {
+            get;
+            private set;
+        }
+
+        private void Awake()
+        {
+            //roomID = FindObjectOfType<Sailing.Online.RoomFactory>().RoomIDReturn();
+            //OnJoinedRoom();
+        }
+
         public override void OnJoinedRoom()
         {
             base.OnJoinedRoom();
 
-            //RoomID.text = "ルームID : " + PhotonNetwork.CurrentRoom.Name;
+            RoomID.text = "ルームID : " + PhotonNetwork.CurrentRoom.Name;
+            //RoomID.text = "ルームID : " + roomID;
             //Debug.Log("RoomID : " + roomID);
 
         }
