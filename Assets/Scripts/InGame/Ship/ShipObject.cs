7996dcafd7a7f8941401f7e3e521898224d557df
phylos096 <43868600+phylos096@users.noreply.github.com>
2021-01-25 14:30:13 +0900

    [update]GameViewCameraのエラー一時的に解消。SettingPrefabの更新

--- Assets/Scripts/InGame/Ship/ShipObject.cs
+++ Assets/Scripts/InGame/Ship/ShipObject.cs
@@ -110,7 +110,7 @@ namespace Sailing
                 child.transform.position = gameObject.transform.position + new Vector3(0.0f, 4.0f, -10.0f);
                 child.transform.LookAt(gameObject.transform);
 
-                child.AddComponent<PostProcessLayer>();//shipにPostProcessLayerを追加する
+                //child.AddComponent<PostProcessLayer>();//shipにPostProcessLayerを追加する
                 child.GetComponent<PostProcessLayer>().volumeLayer = 1<<9;//9番レイヤーのみ指定
                 child.GetComponent<PostProcessLayer>().volumeTrigger = child.gameObject.transform;//TargetをGameViewCameraに指定
                 
