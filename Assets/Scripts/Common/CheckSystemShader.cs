5878fae2a90f103e898c284dc9da800da47f87e3
phylos096 <43868600+phylos096@users.noreply.github.com>
2021-02-05 01:11:04 +0900

    [update]SystemInfoのやつが対応してるか調べる。対応してない場合、旧海を表示する。

---
+++ Assets/Scripts/Common/CheckSystemShader.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckSystemShader : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Water;
+
+    [SerializeField]
+    GameObject Ocean;
+
+    [SerializeField]
+    GameObject OceanInputs;
+
+    private void Awake()
+    {
+
+        //Sceneが遷移しても破棄されないようにする。
+        //DontDestroyOnLoad(this);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        try
+        {
+            Water = GameObject.Find("Water");
+            Ocean = GameObject.Find("Ocean");
+            OceanInputs = GameObject.Find("OceanInputs");
+
+            if (SystemInfo.supportsComputeShaders)
+            {
+                Water.SetActive(false);
+            }
+            else
+            {
+                Water.SetActive(true);
+            }
+
+        }
+        catch
+        {
+
+        }
+
+
+    }
+
+    bool checkSystem()
+    {
+        if (!SystemInfo.supportsComputeShaders) return false;
+
+
+        return true;
+    }
+
+}
